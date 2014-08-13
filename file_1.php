efa2db012937b758bea0ae690132159021c38008
Bruno Barban <brunobarban@gmail.com>
2014-08-13 20:49:39 -0300

    fix conflict

--- file_1.php
+++ file_1.php
@@ -1,6 +1,6 @@
 <?php
-	$my_array_3 = array('black', 'orange', 'yellow', 'white', 'red');
+	$my_array_x = array('black', 'orange', 'yellow', 'white', 'red');
 	
-	foreach($my_array_3 as $item) {
+	foreach($my_array_x as $item) {
 		print $item . '</ br>';
 	}
